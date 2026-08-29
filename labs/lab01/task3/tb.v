1b548b68a04a63aaa5e29f1f428b6681de3a59a5
krishgta <f20241278@pilani.bits-pilani.ac.in>
2026-08-29 16:28:17 +0000

    lab01 task3

--- labs/lab01/task3/tb.v
+++ labs/lab01/task3/tb.v
@@ -33,16 +33,9 @@ module tb;
     #20 t_a = 4'b0101; t_b = 4'b0011; t_cin = 1;
     #20 t_a = 4'b1010; t_b = 4'b0101; t_cin = 0;
     #20 $finish;
-
-
-
-
   end
 
   initial
     $monitor($time, " a=%b b=%b cin=%b | sum=%b cout=%b", t_a, t_b, t_cin, t_sum, t_cout);
-
-
-    
 
 endmodule
