997764e5c0404afef367531f05da8381d143f8a0
krishgta <f20241278@pilani.bits-pilani.ac.in>
2026-09-19 10:42:00 +0000

    lab02 task3

--- labs/lab02/task3/comp2.v
+++ labs/lab02/task3/comp2.v
@@ -1,19 +1,16 @@
 // comp2.v
-// 2-bit unsigned magnitude comparator.
+// 2-bit unsigned magnitude comparator
 
 module comp2 (
-  input  [1:0] A,
-  input  [1:0] B,
-  output       GT,
-  output       LT,
-  output       EQ
-
+  input  wire [1:0] A,
+  input  wire [1:0] B,
+  output wire GT,
+  output wire LT,
+  output wire EQ
 );
 
-  assign EQ = (A == B);
-  
   assign GT = (A > B);
-
   assign LT = (A < B);
+  assign EQ = (A == B);
 
 endmodule
